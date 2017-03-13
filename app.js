6fb16de4ffdf92acd1afc39ecea93924c5af6ede
Jun <jlei91@gmail.com>
2017-03-13 05:26:38 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,164 @@
+var state = {
+
+  URL: {
+    league: "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion",
+    youtube: 'https://www.googleapis.com/youtube/v3/search',
+  },
+  currentskin: 0,
+}
+
+function getDataFromLeagueApi(searchTerm, callback) {
+  var query = {
+    champData: 'info,skins',
+    api_key: 'RGAPI-b2ae3075-552d-4b32-bcd3-7523ec3fe407',
+  }
+  $.getJSON(state.URL.league, query, callback);
+}
+
+function getDataFromYoutubeApi(searchTerm, callback) {
+  var query = {
+    q: searchTerm,
+    part: 'snippet',
+    key: 'AIzaSyCLzHHWZfMw2w1o5g4OrfkatluyOunoOOc',
+  }
+  $.getJSON(state.URL.youtube, query, callback);
+}
+// make a fixed navbar, with search bar that will be up there with it
+//put the URLs in a state object, and currentskin
+
+function firstLetterUppercase(str) { //rename 
+  var splitStr = str.toLowerCase().split(' '); //rename
+  var arr = [];
+
+  for (i = 0; i < splitStr.length; i++) {
+    arr.push(splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1));
+  } //try using arr.map 
+  // word.split(' ').map(s => s.charAt(0).toUpperCase() + s.slice(1)).join(' ');
+  return arr.join(' ');
+}
+function displaySkinName(obj) { //change name, i.e displayskintype
+  var resultElement = '';
+  var optionElement = '';
+  var userInput = $('input.js-query').val();
+  userInput = firstLetterUppercase(userInput);
+
+  for (var i in obj.data) {
+    if (obj.data[i].name === userInput) {
+      for (var y = 0; y < obj.data[i].skins.length; y++) {
+        if (y > 0) {
+          resultElement +=
+            '<li class = "skinName col-xs-12 col-md-2" value =' + y + '>' + obj.data[i].skins[y].name + '</li>';
+          optionElement +=
+            '<option class = "skinName" value =' + y + '>' + obj.data[i].skins[y].name + '</option>';
+        }
+      }
+    }
+  }
+  $('.js-search-results').html(resultElement);
+  $('select').html(optionElement);
+}
+
+function renderSplashArt(obj) {
+  var resultElement = '<div = "splashart" style = "text-align:center ">Splash Art</div>';
+  var userInput = $('input.js-query').val(); //put in state, ie.currentquery
+  userInput = firstLetterUppercase(userInput);
+  for (var i in obj.data) {
+    if (obj.data[i].name === userInput) {
+      for (var y = 0; y < obj.data[i].skins.length; y++) {
+        if (y > 0 && y == state.currentSkin) { //refractor!!
+          resultElement +=
+            '<img class = "splash" src = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' +
+            obj.data[i].key + '_' + y + '.jpg">';
+        }
+      }
+    }
+  }
+  $('.js-splash').append(resultElement);
+}
+
+function renderVideoResult(data) {
+  var resultElement = '<div class = "youtubevid" style = "text-align:center">Champion Skin Spotlights</div>';
+  if (data.items) {
+    console.log(data.items);
+    
+    data.items.forEach(function (item) {
+      resultElement += '<div class = "videoName col-md-4 col-xs-12"><div class ="panel-body"><a class = "youtubeLink" href ="#" data-toggle="modal" data-target="#videoModal" data-theVideo=https://youtube.com/embed/' + item.id.videoId + '><img class = "col-xs-12" src =http://i.ytimg.com/vi/' + item.id.videoId + '/maxresdefault.jpg' + '></a></div></div>';
+    });
+  }
+  else {
+    resultElement += '<p>Sorry... nothing came up</p>';
+  }
+  $('div.panel.panel-default').html(resultElement);
+}
+
+function searchSkins(champName) {
+  getDataFromYoutubeApi(champName, renderVideoResult);
+}
+
+function watchSubmit() {
+  $('.js-search-form').submit(function (e) {
+    e.preventDefault();
+    $('p').addClass('show');
+    //$('#skins').addClass('show');
+    $('.js-splash').empty();
+    $('.js-video').empty();
+    getDataFromLeagueApi($(this).find('.js-query').val(), displaySkinName
+    );
+  });
+}
+
+//FUNCTION TO GET AND AUTO PLAY YOUTUBE VIDEO FROM DATATAG
+function autoPlayYouTubeModal(clickedVideo) {
+  console.log(clickedVideo.attr("data-theVideo"));
+  console.log('target:' + clickedVideo.data("target"));
+  var theModal = clickedVideo.data("target"),
+    videoSRC = clickedVideo.attr("data-theVideo"),
+    videoSRCauto = videoSRC + "?autoplay=1";
+  $(theModal + ' iframe').attr('src', videoSRCauto);
+  $(theModal + ' button.close').click(function () {
+    $(theModal + ' iframe').attr('src', videoSRC);
+  });
+  $(theModal).on('hidden.bs.modal', function () {
+    $(theModal + ' iframe').attr('src', videoSRC);
+  });
+};
+
+$('.js-search-results').on('click', '.skinName', function (e) {
+  $('.js-splash').empty();
+  $('.js-video').empty();
+  var chosenSkin = $(this).text();
+  searchSkins(chosenSkin + " League of Legends skin spotlight");
+  getDataFromLeagueApi(chosenSkin, renderSplashArt);
+  state.currentSkin = $(this).val();
+});
+
+$('#skins').on('change', function () {
+  $('.js-splash').empty();
+  $('.js-video').empty();
+  var chosenSkin = $(this).find("option:selected").text(); 
+  console.log($(this).find("option:selected").text() + ' clicked!');
+  searchSkins(chosenSkin + " League of Legends skin spotlight");
+  getDataFromLeagueApi(chosenSkin, renderSplashArt);
+  state.currentSkin = $(this).val();
+});
+
+$('.panel').on('click', '.youtubeLink', function (e) {
+  autoPlayYouTubeModal($(this));
+});
+
+watchSubmit();
+
+// $('.js-query').keydown(function () {
+//   console.log("key is down!");
+//   $('.js-search-form').css({ position: 'absolute' }).animate({
+//     bottom: 158
+//   });
+//   $('.box').addClass("removeImage");
+// });
+
+/*
+make video thumbnail not blurry
+make section tags centered
+fix up js
+fix search bar - 
+*/
