6d66708e0943e094637840d1226e57029b7f993f
Jun <jlei91@gmail.com>
2017-03-29 10:30:16 -0700

    added background, loading circle,etc

--- app.js
+++ app.js
@@ -27,46 +27,38 @@ function getDataFromYoutubeApi(searchTerm, callback) {
   $.getJSON(state.URL.youtube, query, callback);
 }
 
-function firstLetterUppercase(str) { //function to split input and uppercase the first letter of each string
-  var splitStr = str.toLowerCase().split(' ');
-  var arr = [];
-  for (i = 0; i < splitStr.length; i++) {
-    arr.push(splitStr[i].charAt(0).toUpperCase() + splitStr[i].slice(1));
-  }
-  return arr.join(' ');
-}
-
-function displayChampionCard(obj) {
-  $('.cardContainer').empty();
-  var stats = '';
-  state.userInput = firstLetterUppercase($('.js-query').val());
-  for (var i in obj.data) {
-    if (obj.data[i].name === state.userInput) {
-      stats += '<div class = "clearfix card"><div class = "hold col-xs-6"><img class ="card-img-top pull-left" src ="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' + obj.data[i].key + '.png"><div class = "card-block"><h4 class = "card-title">' + obj.data[i].name + '</h4><p class = "card-text">' + obj.data[i].title + '</p><div class = "underneath"><p class = "col-xs-6">Health: ' + obj.data[i].stats.hp + ' (+' + obj.data[i].stats.hpperlevel + ' per level)</p><p class = "col-xs-6">Attack Damage: ' + obj.data[i].stats.attackdamage + ' (+' + obj.data[i].stats.attackdamageperlevel + ' per level)</p><p class = "col-xs-6">Movement Speed: ' + obj.data[i].stats.movespeed + '</p><p class = "col-xs-6">Health Regen: ' + obj.data[i].stats.hpregen + ' (+' + obj.data[i].stats.hpregenperlevel + ' per level)</p><p class = "col-xs-6">Armor: ' + obj.data[i].stats.armor + ' (+' + obj.data[i].stats.armorperlevel + ' per level)</p><p class = "col-xs-6">Magic Resist: ' + obj.data[i].stats.spellblock + ' (+' + obj.data[i].stats.spellblockperlevel + ' per level)</p></div></div></div><div class = "loreContainer"><h6 class="col-xs-6">' + obj.data[i].lore + '</h6></div></div>';
-    }
-  }
-  $('.cardContainer').append(stats);
-}
-
 function displayChampionSprite(obj) {
   var grids = '';
+  var arr = [];
   for (var i in obj.data) {
-    if (i === 'Fiddlesticks'){
-       obj.data[i].key = obj.data[i].key.replace('s','S');
+    if (i === 'Fiddlesticks') {
+      obj.data[i].key = obj.data[i].key.replace('s', 'S');
     };
+    arr.push(obj.data[i].name);
+    arr.sort;
     grids =
       '<div class = "col-xs-3 col-md-2 profile"><div class = panel-panel-default"><div class = panel-thumbnail"><img class = "sprite" value = "' + obj.data[i].name + '" src = "http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' + obj.data[i].key + '.png"></a></div></div><div class = championName>' + obj.data[i].name + '</div>';
     $('.character-grid-container').append(grids);
   }
 }
-//fix kogmaw search and other names with comma -- add a or for the condition for commas
+
+function displayChampionCard(obj) {
+  $('.cardContainer').empty();
+  var stats = '';
+  state.userInput = ($('.js-query').val().toLowerCase());
+  for (var i in obj.data) {
+    if (obj.data[i].name.toLowerCase() === state.userInput || obj.data[i].key.toLowerCase() === state.userInput) {
+      stats += '<div class = "clearfix card"><div class = "card-container col-xs-12 col-md-6"><img class ="card-img-top pull-left" src ="http://ddragon.leagueoflegends.com/cdn/6.24.1/img/champion/' + obj.data[i].key + '.png"><div class = "card-block"><h3 class = "card-title">' + obj.data[i].name + '</h3><h4 class = "card-text">' + obj.data[i].title + '</h4><div class = "stats"><p class = "col-xs-6">Health: ' + obj.data[i].stats.hp + ' (+' + obj.data[i].stats.hpperlevel + ' per level)</p><p class = "col-xs-6">Attack Damage: ' + obj.data[i].stats.attackdamage + ' (+' + obj.data[i].stats.attackdamageperlevel + ' per level)</p><p class = "col-xs-6">Movement Speed: ' + obj.data[i].stats.movespeed + '</p><p class = "col-xs-6">Health Regen: ' + obj.data[i].stats.hpregen + ' (+' + obj.data[i].stats.hpregenperlevel + ' per level)</p><p class = "col-xs-6">Armor: ' + obj.data[i].stats.armor + ' (+' + obj.data[i].stats.armorperlevel + ' per level)</p><p class = "col-xs-6">Magic Resist: ' + obj.data[i].stats.spellblock + ' (+' + obj.data[i].stats.spellblockperlevel + ' per level)</p></div></div></div><div class = "loreContainer col-xs-12 col-md-6"><h3 class = "loretitle">Lore</h3><h5>' + obj.data[i].lore + '</h5></div></div>';
+    }
+  }
+  $('.cardContainer').append(stats);
+}
 
 function displaySkinName(obj) { //using userinput to search for champion to display it's skins
   var cardElement = '';
-  // var stats = '';
-  state.userInput = firstLetterUppercase($('.js-query').val());
+  state.userInput = ($('.js-query').val().toLowerCase());
   for (var i in obj.data) {
-    if (obj.data[i].name === state.userInput) {
+    if (obj.data[i].name.toLowerCase() === state.userInput || obj.data[i].key.toLowerCase() === state.userInput) {
       for (var y = 1; y < obj.data[i].skins.length; y++) {
         cardElement +=
           '<div class = "col-md-3 col-xs-12 profile"><div class = "panel panel-default"><div class="panel-thumbnail"><a href= "#" title ="' + obj.data[i].skins[y].name + '" class ="thumb" value=' + y + '><img src = "http://ddragon.leagueoflegends.com/cdn/img/champion/splash/' + obj.data[i].key + '_' + obj.data[i].skins[y].num + '.jpg" value =' + y + ' class = "img-responsive img-rounded splash" data-toggle ="modal" data-target = ".modal-profile-lg"></a></div><div class = "panel-body"><p class ="profile-name" value =' + y + '>' + obj.data[i].skins[y].name + '</p></div></div></div>';
@@ -156,7 +148,6 @@ function watchSubmit() {
       $(this).hide();
     });
     $('.splash').show();
-    state.userInput = $('.js-query').val();
     getDataFromLeagueApi($(this).find('.js-query').val(), displaySkinName);
     getDataFromLeagueApi($(this).find('.js-query').val(), displayChampionCard);
   });
@@ -164,7 +155,6 @@ function watchSubmit() {
 
 $('.js-search-form').on('keyup', '.js-query', function (e) {
   e.preventDefault();
-  $('.cardContainer').empty();
   if (e.keyCode == 8) {
     showJumbotron($(this));
   }
@@ -192,14 +182,12 @@ $('.modal-content').on('click', '.carousel-control', function (e) {
   changeImage($(this));
 });
 
-$('.navbar-brand').click(function (e) {
+$('.navbar-brand').click(function (e) { //reload to loading page
   e.preventDefault();
   location.reload();
 })
 
-$('.championList').on('click', '.sprite', function (e) {
-  console.log('champ clicked');
-  console.log($(this).attr('value'));
+$('.championList').on('click', '.sprite', function (e) { //event listener for sprites on loading page
   var spriteValue = $(this).attr('value');
   $('.js-query').val(spriteValue);
   $('.championList').hide();
@@ -214,8 +202,19 @@ $('.championList').on('click', '.sprite', function (e) {
   getDataFromLeagueApi($(this).find('.js-query').val(), displayChampionCard);
 });
 
-watchSubmit();
+$body = $("body");
+
+$(document).on({
+  ajaxStart: function () {
+    $body.addClass("loading");
+  },
+  ajaxStop: function () {
+    $body.removeClass("loading");
+  }
+});
 
 $(document).ready(function () {
   getDataFromLeagueApi(state.userInput, displayChampionSprite);
 });
+
+watchSubmit();
